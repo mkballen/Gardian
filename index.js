dc2d7eb2d6ea9fc43feae5ec970ab47f4c09d06e
Michael Ballantyne <bal19034@byui.edu>
2022-05-17 11:23:32 -0600

    manifest

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const application = express();
+
+application.get('/',(req,res)=>{res.send("Hello")});
+
+application.listen(3000,()=>{console.log("Listening...")});
