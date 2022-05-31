1d7be24344553c92c9d15e1c9321941b47712a8f
Michael Ballantyne <bal19034@byui.edu>
2022-05-31 11:20:19 -0600

    hello

---
+++ test/loadbalancer.test.js
@@ -0,0 +1,9 @@
+import fetch from 'node-fetch';
+import assert from 'assert';
+//import { response } from 'express';
+
+it ('Should get hello', async ()=>{
+    const response = await fetch('http://34.86.170.196');
+    const text = await response.text();
+    assert.equal(text,"Hello");
+})
